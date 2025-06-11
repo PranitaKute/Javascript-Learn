49db3a07560c0e847a61dc1d28e6f361cf22516c
Pranita Kute <97874560+PranitaKute@users.noreply.github.com>
2025-06-11 12:36:19 +0530

    Static keyword index.js

--- Website/index.js
+++ Website/index.js
@@ -1083,3 +1083,53 @@ console.log(`Total Price (with tax): $${total2}`);
 product3.displayProduct();
 const total3 = product3.calculateTotal(salestax);
 console.log(`Total Price (with tax): $${total3}`);
+
+
+/*Static : keyword that defines properties or methods that belong to
+a class itself rather than the objects created from that class
+(class owns anything static, not the object)*/
+class MathUtil{
+    static PI = 3.14159;
+    static getDiameter(radius){
+        return radius * 2;
+    }
+    static getCircumference(radius){
+        return 2 * this.PI * radius;
+    }
+    static getArea(radius){
+        return this.PI * radius * radius;
+    }
+}
+console.log(MathUtil.PI);
+console.log(MathUtil.getDiameter(10));
+console.log(MathUtil.getCircumference(10));
+console.log(MathUtil.getArea(10));
+
+class Users{
+    static userCount = 0;
+    constructor(username){
+        this.username = username;
+        Users.userCount += 1;
+    }
+    static getUserCount(){
+        console.log(`There are ${this.userCount} active users`);
+    }
+    sayHello(){
+        console.log(`Hello, My username is ${this.username}`)
+    }
+}
+const user1 = new Users("Spongebob");
+console.log(user1.username);
+console.log(Users.userCount);
+user1.sayHello();
+const user2 = new Users("Patrick");
+console.log(user2.username);
+console.log(Users.userCount);
+user2.sayHello();
+const user3 = new Users("Sandy");
+console.log(user3.username);
+console.log(Users.userCount);
+user3.sayHello();
+
+Users.getUserCount();
+
