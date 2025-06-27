35c88e9662e1107f60b7ffad0904ff9ae0419d60
Pranita Kute <97874560+PranitaKute@users.noreply.github.com>
2025-06-27 17:50:56 +0530

    ES6 Module

---
+++ Website/mathUtil.js
@@ -0,0 +1,13 @@
+export const Pi = 3.14159;
+
+export function getCircumference(radius){
+    return Pi * Pi * radius;
+}
+
+export function getArea(radius) {
+    return Pi * radius * radius;
+}
+
+export function getVolume(radius){
+    return 4 * Pi * radius * radius;
+}
