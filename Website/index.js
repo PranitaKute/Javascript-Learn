4e6a15f88ca3503596ed599c7905f3f4bddf2d49
Pranita Kute <97874560+PranitaKute@users.noreply.github.com>
2025-06-27 18:33:55 +0530

    Asynchronous index.js

--- Website/index.js
+++ Website/index.js
@@ -1654,3 +1654,32 @@ const volume = getVolume(10);
 console.log(`Circumference : ${circumference.toFixed(2)}cm`);
 console.log(`Area : ${area.toFixed(2)}cm²`);
 console.log(`Volume : ${volume.toFixed(2)}cm³`);
+
+
+/*Synchronous - Executes line by line consecutively in a sequential manner.
+Code that waits for an operation to complete.
+
+Asynchronous - Allows multiple operations to be performed concurrently
+without waiting.
+Doesn't block the execution flow and allows the program to continue
+(I/O operations, network requests, fetching data)
+Handled with : Callbacks, Promises, Async/Await*/
+// Synchronous
+console.log("Task 1.1");
+console.log("Task 1.2");
+console.log("Task 1.3");
+
+// Asynchronous function
+// Callback
+function func1(callback){
+    setTimeout(()=>{console.log("Task 1");
+        callback()}, 3000);
+}
+
+function func2(){
+    console.log("Task 2");
+    console.log("Task 3");
+    console.log("Task 4");
+}
+
+func1(func2);
