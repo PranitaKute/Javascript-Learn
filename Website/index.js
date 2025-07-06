6faad7b636de2eb0152b5c79de77cce681a5df8f
Pranita Kute <97874560+PranitaKute@users.noreply.github.com>
2025-07-06 12:15:08 +0530

    Callback Hell index.js

--- Website/index.js
+++ Website/index.js
@@ -2082,3 +2082,43 @@ b.forEach(button => {
         }
     });
 });
+
+
+/*Callback Hell - Situation in javascript where callbacks are nested 
+within other callbacks to the degree where the code is diffcult to read. 
+Old patteren to handle asynchronous functions. 
+Use promises + async/await to avoid Callback Hell*/
+function task1(callback){
+    setTimeout(() => {
+        console.log("Task 1 Complete");
+        callback();
+    },2000);
+}
+function task2(callback){
+    setTimeout(() => {
+        console.log("Task 2 Complete");
+        callback();
+    },1000);
+}
+function task3(callback){
+    setTimeout(() => {
+        console.log("Task 3 Complete");
+        callback();
+    },3000);
+}
+function task4(callback){
+    setTimeout(() => {
+        console.log("Task 4 Complete");
+        callback();
+    },1500);
+}
+
+task1(() => {
+    task2(() => {
+        task3(() => {
+            task4(() => {
+                console.log("All tasks Completed");
+            });
+        });
+    });
+});
